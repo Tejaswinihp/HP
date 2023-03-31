99d9bd5ace2e593743ea3b2ad20bc04fd384b3f5
root <root@ip-172-31-13-159.ap-south-1.compute.internal>
2023-03-31 14:56:34 +0000

    initial

---
+++ apple.sh
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+import java.util.Scanner;
+class Input {
+    public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+    	System.out.println("Enter your name: ");
+    	String inp = input.next();
+    	System.out.println("Hello, " + inp);
+    }
